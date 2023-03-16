de86942d56869800fb0781a7f1e3e03fb16f72ae
Diego Hernandez <A00834015@tec.mx>
2023-03-16 02:18:50 -0600

    Added ros class for the algorithm:  - Sensor subscribers  - Service clients for map resetting  - Action client for move base goals

--- maze_ws/src/exploration/src/actionClient.cpp
+++ maze_ws/src/exploration/src/actionClient.cpp
@@ -29,9 +29,19 @@ struct ClientScope
 
 ClientScope scope;
 
+int getColor()
+{
+    return 0;
+}
+
+void recovery()
+{
+    ROS_INFO("Recovery");
+}
+
 void doneCb(const actionlib::SimpleClientGoalState &state, const move_base_msgs::MoveBaseResult::ConstPtr &result)
 {
-    ROS_INFO_STREAM("Finished in state " << state.toString());
+    ROS_INFO_STREAM("done callback: Finished in state " << state.toString());
     // ROS_INFO_STREAM("Answer: " << result->sequence.back());
 
     scope.result = state.state_;
@@ -45,14 +55,15 @@ void doneCb(const actionlib::SimpleClientGoalState &state, const move_base_msgs:
 // void feedbackCB(const move_base_msgs::MoveBaseFeedbackConstPtr &feedback)
 void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr &feedback)
 {
-    // ROS_INFO_STREAM("Feedback x: " << feedback->base_position.pose.position.x);
-    // ROS_INFO_STREAM("Feedback y: " << feedback->base_position.pose.position.y);
-    // ROS_INFO_STREAM("Feedback z: " << feedback->base_position.pose.position.z);
-    // ROS_INFO_STREAM("GoalID: " << feedback->status.goal_id.id);
-    // ROS_INFO_STREAM("x: " << feedback->feedback.base_position.pose.position.x);
-    // ROS_INFO_STREAM("y: " << feedback->feedback.base_position.pose.position.y);
-    // ROS_INFO_STREAM("z: " << feedback->feedback.base_position.pose.position.z);
-    // ROS_INFO_STREAM("Got Feedback of length " << feedback->sequence.size());
+
+    ROS_INFO("Feedback x %f", feedback->base_position.pose.position.x);
+    ROS_INFO("Feedback y %f", feedback->base_position.pose.position.y);
+    ROS_INFO("Feedback z %f", feedback->base_position.pose.position.z);
+
+    if (getColor() == 1)
+    {
+        
+    }
 
     scope.feedback = feedback->base_position.pose.position.x;
 
@@ -69,21 +80,11 @@ void activeCb()
 void statusCallback(const actionlib_msgs::GoalStatusArray::ConstPtr &msg)
 {
     // ROS_INFO_STREAM("Status: " << msg->goal_id.id);
-    ROS_INFO("Status: %d", msg->status_list[0].status);
+    ROS_INFO("StatusCallback: %d", msg->status_list[0].status);
     if (scope.startedGoal)
         scope.status = msg->status_list[0].status;
     // ROS_INFO_STREAM("Status: " << msg->status_list[0].text);
     // ROS_INFO_STREAM("Status: " << msg->status_list[0].goal_id.id);
-}
-
-int getColor()
-{
-    return 0;
-}
-
-void recovery()
-{
-    ROS_INFO("Recovery");
 }
 
 void movementClientAsync(move_base_msgs::MoveBaseGoal goal)
@@ -102,33 +103,31 @@ void movementClientAsync(move_base_msgs::MoveBaseGoal goal)
 
     ac.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
 
+    ac.waitForResult();
     scope.resultReceived = false;
-    scope.status = actionlib_msgs::GoalStatus::PENDING;
+    // scope.status = actionlib_msgs::GoalStatus::PENDING;
 
     // ros::spinOnce();
 
-    while (ros::ok() && !scope.resultReceived && 
-        scope.status != actionlib_msgs::GoalStatus::PREEMPTED && 
-        scope.status != actionlib_msgs::GoalStatus::SUCCEEDED && 
-        scope.status != actionlib_msgs::GoalStatus::ABORTED && 
-        scope.status != actionlib_msgs::GoalStatus::REJECTED && 
-        scope.status != actionlib_msgs::GoalStatus::RECALLED)
+    // while (ros::ok() && scope.resultReceived)
     
     //(scope.status == actionlib_msgs::GoalStatus::ACTIVE || scope.status == actionlib_msgs::GoalStatus::PENDING || scope.status == actionlib_msgs::GoalStatus::RECALLED || scope.status == actionlib_msgs::GoalStatus::RECALLING))
-    {
-        ROS_INFO("in movement client async loop, Status: %d", scope.status);
-        ROS_INFO("resultReceived: %d", scope.resultReceived);
+    // {
+    //     ROS_INFO("in movement client async loop, Status: %d", scope.status);
+    //     ROS_INFO("resultReceived: %d", scope.resultReceived);
         
-        if (false)
-        {
-            ac.cancelAllGoals();
-            recovery();
+    //     if (false)
+    //     {
+    //         ac.cancelAllGoals();
+    //         recovery();
 
-            break;
-        }
+    //         break;
+    //     }
 
-        ros::spinOnce();
-    }
+    //     ros::Duration(0.1).sleep();
+
+    //     ros::spinOnce();
+    // }
 
     ROS_INFO("movementClientAsync result: %d", scope.status);
     return;
@@ -212,13 +211,16 @@ int main(int argc, char **argv)
     // ros::spinOnce();
 
     handleUnitMovements(0);
-    while (ros::ok() && !scope.resultReceived)
-    {
-        ros::spinOnce();
+    // while (ros::ok() && !scope.resultReceived)
+    // {
 
-        ROS_INFO("Result received: %d", scope.resultReceived);
-        ROS_INFO("Status: %d", scope.status);
-    }
+    //     ROS_INFO("In main while, Result received: %s", (scope.resultReceived) ? "true" : "false");
+    //     ROS_INFO("Status: %d", scope.status);
+
+    //     ros::Duration(0.1).sleep();
+
+    //     ros::spinOnce();
+    // }
     
     // while (ros::ok() && !scope.resultReceived && scope.status != actionlib_msgs::GoalStatus::SUCCEEDED && scope.status != actionlib_msgs::GoalStatus::ABORTED && scope.status != actionlib_msgs::GoalStatus::REJECTED && scope.status != actionlib_msgs::GoalStatus::RECALLED)
     // {
