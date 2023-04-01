737ad41564ee26f5381f6fa7acfa1c84318dbb8a
Diego Hernandez <A00834015@tec.mx>
2023-04-01 10:19:47 -0600

    primera version
    Co-authored-by: Oscar Arreola <Oscar-gg@users.noreply.github.com>

--- navSensors/main_code/main_code.ino
+++ navSensors/main_code/main_code.ino
@@ -1,6 +1,8 @@
 // Usar bno y vlx para robot.
 // Tmb poner el dispense
 
+// https://prod.liveshare.vsengsaas.visualstudio.com/join?AB1AC08928A0F68DBD97A45B571C4353E54F
+
 #include "Movement.h"
 #include "Sensors.h"
 #include "Motor.h"
@@ -12,36 +14,64 @@ Movement *robot = nullptr;
 Sensors *s = nullptr;
 MUX2C mux;
 
+double distancefront;
+double distanceright;
+double distanceleft;
+
+double yaw;
+
+double northYaw;
+double southYaw;
+double eastYaw;
+double westYaw;
+
+bool frontBlack = false;
+bool rightBlack = false;
+bool leftBlack = false;
+
+char color = 'B';
+
 // Setup de todos los sensores. Set pins en Sensors.h
 void setup()
 {
   Serial.begin(57600);
-  
+
   // Set some sensor or i2c device
   bool setTcs = false;
   bool seti2c = false;
   bool setVLX = false;
   bool setBNO = false;
+  bool testMotors = false
 
   initAll(bno, true, true);
-  
-  setupData(setTcs, seti2c, setVLX, setBNO);
-  
+
+  setupData(setTcs, seti2c, setVLX, setBNO, testMotors);
+
   bno.init();
-  
+
+  northYaw = bno.getAngleZ();
+
+  // East
+  if (northYaw > -90)
+    eastYaw = northYaw - 90;
+  else
+    eastYaw = northYaw + 90 * 3;
+
+  // South
+  if (northYaw > 0)
+    southYaw = northYaw - 180;
+  else
+    southYaw = northYaw + 180;
+
+  // West
+  if (northYaw > 90)
+    westYaw = northYaw - 90 * 3;
+  else
+    westYaw = northYaw + 90;
 }
-
-double distancefront;
-double distanceright;
-double distanceleft;
-
-bool frontBlack = false;
-
-char color = 'B';
 
 void loop()
 {
-
   // Follow right wall
   while (distancefront > 0.15 && color != 'N' && distanceright < 0.15)
   {
@@ -50,33 +80,57 @@ void loop()
     distanceright = sensors->getVLXInfo();
 
     color = sensors->getTCSInfo();
-    
-    // Go forward
-    if (color == 'A') // Blue tile
+
+    // Check limit switches
+    if (robot->rightLimitSwitch)
+    {
+      // Go back and turn left
+      robot->advanceXMeters(-0.03)
+          robot->girarDeltaAngulo(10)
+    }
+    else if (robot->leftLimitSwitch)
+    {
+      // Go back and turn right
+      robot->advanceXMeters(-0.03)
+          robot->girarDeltaAngulo(-10)
+    }
+    else if (color == 'A') // Blue tile
     {
       // Go forward and wait 5 seconds
-      
-      
-      
+      robot->advanceXMeters(0.1);
+      delay(5000);
     }
     else if (color == 'N')
     {
       // Go backward
+      robot->advanceXMeters(-0.1);
 
-
+      frontBlack = true;
     }
 
-    // Check distancehttps://prod.liveshare.vsengsaas.visualstudio.com/join?AB1AC08928A0F68DBD97A45B571C4353E54F
-
+    // Check distance
     if (distanceright > 0.15)
     {
       // Turn right
+      robot->girarDeltaAngulo(90);
+
+      if (frontBlack)
+      {
+        frontBlack = false;
+        leftBlack = true;
+      }
     }
-    else if (distancefront < 0.15)
+    else if (distancefront < 0.10 || frontBlack)
     {
       // Turn left
+      robot->girarDeltaAngulo(-90);
+
+      frontBlack = false;
+      rightBlack = true;
     }
-    
+
+    // Go forward
+    robot->advanceXMeters(0.1);
   }
 }
 
@@ -93,7 +147,7 @@ void initAll(BNO *bno, bool useVLX, bool setIndividualConstants)
 // Funciones de apoyo
 
 // Funcion que ayuda para calibrar sensores, encontrar idc.
-void setupData(bool tcsSet, bool i2c, bool setVLX, bool setBNO)
+void setupData(bool tcsSet, bool i2c, bool setVLX, bool setBNO, bool testMotors)
 {
 
   if (i2c)
@@ -127,6 +181,9 @@ void setupData(bool tcsSet, bool i2c, bool setVLX, bool setBNO)
       bno->anglesInfo();
     }
   }
+
+  if (testMotors)
+    robot->testAllMotors();
 
   if (i2c)
     while (true)
