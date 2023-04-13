8290bfe2935784183f7f0eddf9f42180e8909ea8
Oscar Arreola Linux <oscar.arreola.jr@gmail.com>
2023-04-12 18:03:44 -0600

    Changed ports for openmvs

--- maze_ws/src/devices/openmv_camera/scripts/camera_wrapper.py
+++ maze_ws/src/devices/openmv_camera/scripts/camera_wrapper.py
@@ -13,44 +13,50 @@ class CameraWrapper:
     def __init__(self):
         # Initialize ROS node with a unique name
         rospy.init_node('camera_server')
+        if (debug): rospy.loginfo("Open mv camera wrapper initialized")
         
         # Wait for the other services to be available before continuing
         rospy.wait_for_service('openmv_camera_left')
         rospy.wait_for_service('openmv_camera_right')
-        rospy.wait_for_service('get_walls_dist')
+        # rospy.wait_for_service('get_walls_dist')
 
         # Create a client to call the other service
         self.get_left_camera = rospy.ServiceProxy('openmv_camera_left', CameraDetection)
         self.get_right_camera = rospy.ServiceProxy('openmv_camera_right', CameraDetection)
-        self.get_dist = rospy.ServiceProxy('get_walls_dist', GetWalls)
+        # self.get_dist = rospy.ServiceProxy('get_walls_dist', GetWallsDist)
 
         # Create a server to handle service calls
         self.my_service_server = rospy.Service('get_victims', BothCameras, self.handle_camera)
+
 
     def handle_camera(self, request):
         # Call the other service
         left_camera = 'X'
         right_camera = 'X'
 
-        distances = self.get_dist()
-
+        #distances = self.get_dist()
+        distances = GetWallsDistResponse()
+        distances.left = 0.2
+        distances.right = 0.2
+        rospy.loginfo("Distances: " + str(distances.left) + "m, " + str(distances.right) + "m")
+        
         # If distances are appropriate, call the camera service
         if distances.left < 0.3:
-            if (debug): print("Calling left camera service")
-            left_camera = self.get_left_camera()
+            if (debug): rospy.loginfo("Calling left camera service")
+            left_camera = self.get_left_camera().char_detection
         else:
-            if (debug): print("Not calling left camera service. Distance: " + str(distances.left) + "m")
+            if (debug): rospy.loginfo("Not calling left camera service. Distance: " + str(distances.left) + "m")
         if distances.right < 0.3:
-            if (debug): print("Calling left camera service")
-            right_camera = self.get_right_camera()
+            if (debug): rospy.loginfo("Calling left camera service")
+            right_camera = self.get_right_camera().char_detection
         else:
-            if (debug): print("Not calling right camera service. Distance: " + str(distances.right) + "m")
+            if (debug): rospy.loginfo("Not calling right camera service. Distance: " + str(distances.right) + "m")
 
         return BothCamerasResponse(left_camera, right_camera)
        
 
 if __name__ == '__main__':
-    # Initialize the MyServiceNode class and spin the node
+    # Initialize the CameraWrapper class and spin the node
     
     name = rospy.get_name() + "/"
     debug = False
